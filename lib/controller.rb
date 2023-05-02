5a0edc9bb47e16f92cebd1741414328b84fe5ca8
Camille Lenormand <camille.lenormand02@gmail.com>
2023-05-02 08:21:24 +0200

    update self.id method

--- lib/controller.rb
+++ lib/controller.rb
@@ -25,8 +25,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/gossips/:id' do
-    gossip = Gossip.find(params["id"])
-    erb :"show"
+    erb :"show",  locals: {gossip: Gossip.find(params['id'])}
   end
 
 
